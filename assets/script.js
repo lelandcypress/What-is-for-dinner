cc98d9e4c3ca0d6b49cec1d5df98b2aabac4230b
Michael Souter <souterm1@gmail.com>
2021-06-16 00:04:06 -0400

    Made some good progress on the fetch request. I refactored the code to use jQuerry, I now have the fetch request dynamically populating the recipe title and picture. The picture I have turned into a link which takes you to another page hosting the recipe. Right now I'm trying to get the recipe items to add, but have hit a stopping point. Once I have that knocked out, I'll tackle the second API.

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,30 @@
 var proteinbuttonEl = document.getElementById("proteinbutton");
 var starchbuttonEl = document.getElementById("starchbutton");
-var beefEl  = document.getElementById("beef")
-var chickenEl  = document.getElementById("chicken")
-var fishEl  = document.getElementById("fish")
-var porkEl  = document.getElementById("pork")
-var riceEl  = document.getElementById("rice")
-var potatoesEl  = document.getElementById("potatoes")
-var beansEl  = document.getElementById("beans")
-var noodlesEl  = document.getElementById("noodles")
+var beefEl = document.getElementById("beef");
+var chickenEl = document.getElementById("chicken");
+var fishEl = document.getElementById("fish");
+var porkEl = document.getElementById("pork");
+var riceEl = document.getElementById("rice");
+var potatoesEl = document.getElementById("potatoes");
+var beansEl = document.getElementById("beans");
+var noodlesEl = document.getElementById("noodles");
 
-
-
-var userInput;
 var container = document.getElementById("main-container");
 var mainTitle = document.getElementById("main-title");
-
+var mealEL = $("#render-meal");
 
 // choose protein button
-proteinbuttonEl.addEventListener("click", starchChoice);
-  const 
-
-
+//proteinbuttonEl.addEventListener("click", starchChoice);
 
 // Starch Choice
 
-
-
-
 // choose starch button
-
-
-
-
-
-var genericVar = document.getElementById("food-search");
-var searchBTN = document.getElementById("searchBtn");
-var renderMeal = document.getElementById("render-meal");
-
-searchBTN.addEventListener("click", function () {
-  userChoice = genericVar.value.trim();
+$("#searchBtn").click(function () {
+  userChoice = $("#food-search").val();
   getResults(userChoice);
 });
 
-function getResults(userChoice) {
+function getResults() {
   var getUrl =
     "https://api.edamam.com/api/recipes/v2/?app_id=99ca5904&app_key=c00c47bd8a69967e0b0559fe7d599651&type=public&q=" +
     userChoice +
@@ -55,8 +37,17 @@ function getResults(userChoice) {
       var random = Math.floor(Math.random() * 20);
       var returnedRecipe = data.hits[random];
       console.log(returnedRecipe);
-      renderMeal.textContent = returnedRecipe.recipe.label;
+      var recipeImage = returnedRecipe.recipe.image;
+      var recipeLink = returnedRecipe.recipe.url;
+
+      $("#recipe-title").text(returnedRecipe.recipe.label);
+      $("#recipe-picture").attr("src", recipeImage);
+      $("#recipe-link").attr("href", recipeLink);
+
+      for (var i = 0; i < returnedRecipe.recipe.ingredients.length; i++) {
+        console.log(i++);
+        var recipeList = returnedRecipe.recipe.ingredients[i].text;
+        $("#ingredients").append("<li></li>").text(recipeList);
+      }
     });
 }
-
-
